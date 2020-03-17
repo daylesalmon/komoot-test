0cf01c12a86886b3d69dafaab6bfb145fc4a606e
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-17 17:39:44 +0000

    Map is now showing

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import L from 'leaflet';
 import s from './Map.module.scss';
 
 const Map = () => {
-  return <div className={s.map}>Map goes here</div>;
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    mapRef.current = L.map('map', {
+      center: [49.8419, 24.0315],
+      zoom: 16,
+      layers: [
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        })
+      ]
+    });
+  }, []);
+
+  return <div id="map" className={s.map} ref={mapRef} title="Komoot map" />;
 };
 
 export default Map;
