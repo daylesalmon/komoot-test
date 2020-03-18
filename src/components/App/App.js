db7fc33926289105b399cb90a1a965dbd245ffc8
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-18 21:51:02 +0000

    Moved state a level higher

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LeftPane from 'components/LeftPane/LeftPane';
 import Map from 'components/Map/Map';
 
 function App() {
+  const [points, setPoints] = useState([]);
+
   return (
     <>
-      <LeftPane /> <Map />
+      <LeftPane /> <Map points={points} setPoints={setPoints} />
     </>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,15 +1,17 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
 import L from 'leaflet';
 import s from './Map.module.scss';
 
-const Map = () => {
+const Map = ({ points, setPoints }) => {
   const mapRef = useRef(null);
-  const [points, setPoints] = useState([]);
 
+  // useEffect to set map up
   useEffect(() => {
     mapRef.current = L.map('map', {
-      center: [46.3605683, 13.8164072],
+      center: [46.3605683, 13.8164072], // Center to Triglav
       zoom: 16,
+      // Set layer to OSM (free)
       layers: [
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -17,6 +19,7 @@ const Map = () => {
       ]
     });
 
+    // remove map if 'unmount'
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
@@ -24,28 +27,40 @@ const Map = () => {
     };
   }, []);
 
+  // useEffect for click event/markers
   useEffect(() => {
+    // Set onclick Event
     const onMapClick = e => {
       setPoints([...points, e.latlng]);
-
-      console.log({ points });
+      // Set up a divIcon
       const icon = L.divIcon({
         className: s.circleIcon,
         iconSize: 30,
-        html: points.length
+        html: points.length + 1
       });
 
-      L.marker(e.latlng, { icon }).addTo(mapRef.current);
+      L.marker(e.latlng, { icon }).addTo(mapRef.current); // Create marker at clicked point, and add divIcon created above
     };
 
-    mapRef.current.on('click', onMapClick);
+    mapRef.current.on('click', onMapClick); // Event handler for map click
 
     return () => {
-      mapRef.current.off('click', onMapClick);
+      mapRef.current.off('click', onMapClick); // Remove map click event handler
     };
+  }, [points, setPoints]);
+
+  // useEffect for drawing polyLine
+  useEffect(() => {
+    // Add polyline based on the coords in state
+    L.polyline(points, { color: '#0d8ce7', weight: 10 }).addTo(mapRef.current);
   }, [points]);
 
   return <div id="map" className={s.map} ref={mapRef} title="Komoot map" />;
+};
+
+Map.propTypes = {
+  points: PropTypes.shape.isRequired,
+  setPoints: PropTypes.func.isRequired
 };
 
 export default Map;
