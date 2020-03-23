b5c1c54d56843c5ed57ecad986fe4c55951a2191
Dayle Salmon <dayle.salmon@wmca.org.uk>
2020-03-23 08:50:33 +0000

    Updated download button so it is a button and not a tag by default

--- src/components/LeftPane/LeftPane.js
+++ src/components/LeftPane/LeftPane.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 // Import context
 import { WaypointsContext } from 'globalState/WaypointsContext';
 // Import components
@@ -8,10 +8,9 @@ import s from './LeftPane.module.scss';
 
 const LeftPane = () => {
   const [waypoints] = useContext(WaypointsContext);
-  const [gpxFile, setGpxFile] = useState();
 
-  useEffect(() => {
-    // Set up xml for gpx file (loop through each latlng and put in as a trkpnt / join '' so it doesn't add comma)
+  const onClick = () => {
+    // Set up xml for gpx file (loop through each latlng and put in as a trkpnt / join '\n' so it adds new line rather than comma)
     const gpxData = `<?xml version="1.0" encoding="UTF-8"?>
     <gpx creator="Komoot Route Builder" version="1.0">
       <trk>
@@ -23,13 +22,20 @@ const LeftPane = () => {
                 `<trkpt lat="${waypoint.latlng.lat}" lon="${waypoint.latlng.lng}"><name>${index +
                   1}</name></trkpt>`
             )
-            .join('')}
+            .join('\n')}
         </trkseg>
       </trk>
-    </gpx>`; // Replace all spaces with nothing (whitespace trim)
+    </gpx>`;
 
-    setGpxFile(`data:text/xml;charset=utf-8,${encodeURIComponent(gpxData)}`); // Update state of our gpx file which is used by button below
-  }, [waypoints]);
+    // Create invisible elemnent to generate and download file
+    const element = document.createElement('a'); // Create a element
+    element.setAttribute('href', `data:text/xml;charset=utf-8,${encodeURIComponent(gpxData)}`); // set href to our gpx file/data
+    element.setAttribute('download', 'my-komoot-route.gpx'); // Set download name
+    element.style.display = 'none'; // element should be invisible to user
+    document.body.appendChild(element); // Append it to body
+    element.click(); // Simulate a click event to generate the download
+    document.body.removeChild(element); // Remove the created element when done
+  };
 
   return (
     <div className={s.leftPane}>
@@ -37,9 +43,9 @@ const LeftPane = () => {
       <hr className={s.hr} />
       <WaypointList />
       {waypoints.length > 0 && (
-        <a href={gpxFile} download="my-komoot-route.gpx" className={s.downloadRouteBtn}>
+        <button type="button" className={s.downloadRouteBtn} onClick={() => onClick()}>
           Download your route
-        </a>
+        </button>
       )}
     </div>
   );
